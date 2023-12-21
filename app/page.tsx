be2f6b3407e4847eb8191ddd2478eb6a917487b4
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-12-21 01:35:45 -0300

    ✨ Feat: filter product

--- app/page.tsx
+++ app/page.tsx
@@ -1,31 +1,47 @@
-import { client } from "@/sanity/lib/client"
-import { groq } from "next-sanity"
+import { client } from "@/sanity/lib/client";
+import { groq } from "next-sanity";
 
-import { SanityProduct } from "@/config/inventory"
-import { siteConfig } from "@/config/site"
-import { seedSanityData } from "@/lib/seed"
-import { cn } from "@/lib/utils"
-import { ProductFilters } from "@/components/product-filters"
-import { ProductGrid } from "@/components/product-grid"
-import { ProductSort } from "@/components/product-sort"
+
+
+import { SanityProduct } from "@/config/inventory";
+import { siteConfig } from "@/config/site";
+import { seedSanityData } from "@/lib/seed";
+import { cn } from "@/lib/utils";
+import { ProductFilters } from "@/components/product-filters";
+import { ProductGrid } from "@/components/product-grid";
+import { ProductSort } from "@/components/product-sort";
+
+
+
+
 
 interface Props {
   searchParams: {
     date?: string
     price?: string
+    color?: string
+    category?: string
+    size?: string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price } = searchParams
+  const { date = "desc", price, category, color, size } = searchParams
 
   const priceOrder = price ? `| order(price ${price})` : ""
 
   const dateOrder = date ? `| order(_createdAt ${date})` : ""
 
   const order = `${priceOrder}${dateOrder}`
+
+  const productFilter = `_type=="fair-product"`
+  const colorFilter=color?`&&"${color}" in colors`:""
+    const categoryFilter=category?`&&"${category}" in categories`:""
+  const sizeFilter=size?`&&"${size}" in sizes`:""
+
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
   const products = await client.fetch<SanityProduct[]>(
-    groq`*[_type=="fair-product"] ${order} {
+    groq`${filter} ${order} {
   _id,
     _createdAt,
     name,
@@ -37,7 +53,6 @@ export default async function Page({ searchParams }: Props) {
 }
 `
   )
-  console.log(products)
 
   return (
     <div>
--- components/product-filters.tsx
+++ components/product-filters.tsx
@@ -13,64 +13,94 @@ import { Checkbox } from "@/components/ui/checkbox"
 const filters = [
   {
     id: "category",
-    name: "Category",
+    name: "Categoria",
     options: [
-      { value: "bags", label: "Bags" },
-      { value: "belts", label: "Belts" },
-      { value: "gloves", label: "Gloves" },
-      { value: "scarves", label: "Scarves" },
-      { value: "wallets", label: "Wallets" },
+      { label: "Bolsas", value: "bags" },
+      { label: "Cintos", value: "belts" },
+      { label: "Luvas", value: "gloves" },
+      { label: "Cachecóis", value: "scarves" },
+      { label: "Carteiras", value: "wallets" },
     ],
   },
   {
     id: "size",
-    name: "Size",
+    name: "Tamanho",
     options: [
-      { value: "xs", label: "X-Small" },
-      { value: "s", label: "Small" },
-      { value: "m", label: "Medium" },
-      { value: "l", label: "Large" },
-      { value: "xl", label: "X-Large" },
-      { value: "one-size", label: "One Size" },
+      { value: "xs", label: "XS" },
+      { value: "s", label: "S" },
+      { value: "m", label: "M" },
+      { value: "l", label: "L" },
+      { value: "xl", label: "XL" },
+      { value: "one-size", label: "Único" },
     ],
   },
   {
     id: "color",
-    name: "Color",
+    name: "Cor",
     options: [
-      { value: "black", label: "Black" },
-      { value: "blue", label: "Blue" },
-      { value: "brown", label: "Brown" },
-      { value: "green", label: "Green" },
-      { value: "yellow", label: "Yellow" },
+      { label: "Preto", value: "black" },
+      { label: "Azul", value: "blue" },
+      { label: "Marron", value: "brown" },
+      { label: "Verde", value: "green" },
+      { label: "Amarelo", value: "yellow" },
     ],
   },
 ]
 
 export function ProductFilters() {
+  const router = useRouter()
+  const searchParams = useSearchParams()
+  const searchValues = Array.from(searchParams.entries())
+
   return (
     <form className="sticky top-20">
-      <h3 className="sr-only">Categories</h3>
+      <h3 className="sr-only">Categorias</h3>
 
       {filters.map((section, i) => (
         <Accordion key={i} type="single" collapsible>
           <AccordionItem value={`item-${i}`}>
             <AccordionTrigger>
               <span>
-                Section{" "}
-                <span className="ml-1 text-xs font-extrabold uppercase text-gray-400"></span>
+                {section.name}{" "}
+                <span className="ml-1 text-xs font-extrabold uppercase text-gray-400">
+                  {searchParams.get(section.id)
+                    ? `( ${
+                        section.options.filter(
+                          (value) => value.value == searchParams.get(section.id)
+                        )[0].label
+                      }   )`
+                    : ""}
+                </span>
               </span>
             </AccordionTrigger>
             <AccordionContent>
               <div className="space-y-4">
-                {section.options.map((option) => (
+                {section.options.map((option, optIdx) => (
                   <div
                     key={option.value}
                     className="flex items-center space-x-2"
                   >
-                    <Checkbox />
-                    <label className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70">
-                      Label
+                    <Checkbox
+                      checked={searchValues.some(
+                        ([key, value]) =>
+                          key === section.id && value === option.value
+                      )}
+                      id={`filter-${section.id}-${optIdx}`}
+                      onClick={(event) => {
+                        const params = new URLSearchParams(searchParams)
+                        const checked =
+                          event.currentTarget.dataset.state === "checked"
+                        checked
+                          ? params.delete(section.id)
+                          : params.set(section.id, option.value)
+                        router.replace(`/?${params.toString()}`)
+                      }}
+                    />
+                    <label
+                      htmlFor={`filter-${section.id}-${optIdx}`}
+                      className="text-sm font-medium leading-none peer-disabled:cursor-not-allowed peer-disabled:opacity-70"
+                    >
+                      {option.label}
                     </label>
                   </div>
                 ))}
