5c03dc7b23bd7594261d69871b49ce02df350b3d
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-12-21 02:00:40 -0300

    ✨ Feat: product search by name

--- app/page.tsx
+++ app/page.tsx
@@ -22,11 +22,12 @@ interface Props {
     color?: string
     category?: string
     size?: string
+    search?: string
   }
 }
 
 export default async function Page({ searchParams }: Props) {
-  const { date = "desc", price, category, color, size } = searchParams
+  const { date = "desc", price, category, color, size, search } = searchParams
 
   const priceOrder = price ? `| order(price ${price})` : ""
 
@@ -35,11 +36,12 @@ export default async function Page({ searchParams }: Props) {
   const order = `${priceOrder}${dateOrder}`
 
   const productFilter = `_type=="fair-product"`
-  const colorFilter=color?`&&"${color}" in colors`:""
-    const categoryFilter=category?`&&"${category}" in categories`:""
-  const sizeFilter=size?`&&"${size}" in sizes`:""
+  const colorFilter = color ? `&&"${color}" in colors` : ""
+  const categoryFilter = category ? `&&"${category}" in categories` : ""
+  const sizeFilter = size ? `&&"${size}" in sizes` : ""
+  const searchFilter = search ? `&& name match "${search}*"` : ""
 
-  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}]`
+  const filter = `*[${productFilter}${colorFilter}${categoryFilter}${sizeFilter}${searchFilter}]`
   const products = await client.fetch<SanityProduct[]>(
     groq`${filter} ${order} {
   _id,
