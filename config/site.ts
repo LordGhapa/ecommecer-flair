93383bd677866190e54d406567c536ea4a86f113
Felipe Lacerda <99082399+LordGhapa@users.noreply.github.com>
2023-12-22 03:48:21 -0300

    💬 Texts: update

--- config/site.ts
+++ config/site.ts
@@ -4,11 +4,11 @@ export const siteConfig = {
   name: "Flair-Felipe Lacerda",
   description: "Exemplo de um E-commerce",
   footer: [
-    { name: "Home", href: "/" },
+    { name: "Inicio", href: "/" },
     { name: "About", href: "#" },
-    { name: "Terms & Conditions", href: "#" },
-    { name: "Shipping & Return Policy", href: "#" },
-    { name: "Privacy Policy", href: "#" },
+    { name: "Termos & Condiçoes", href: "#" },
+    { name: "Taxas", href: "#" },
+    { name: "Politicas de privacidade", href: "#" },
     { name: "FAQ", href: "#" },
   ],
 }
